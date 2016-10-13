4e841eb644699df5d29c7b921065e00ccc425d80
Arthelon <hsing.daniel@gmail.com>
2016-10-14 07:55:34 +0800

    Added progress bar

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 const copyPaste = require("copy-paste")
 const Tesseract = require("tesseract.js")
+const Progress = require("progress")
 let args = process.argv.slice(2)
 let lang = "eng"
 const usageTmpl = `Command line utility that copies image text to clipboard
@@ -31,8 +32,17 @@ if (args.length == 0 || args.length > 3) {
 }
 
 function recognize(imagePath, lang) {
+	const bar = new Progress("recognizing [:bar] :percent :elapseds", {total: 100})
+	let prev = 0
 	Tesseract.recognize(imagePath, {
 		lang
+	})
+	.progress(p => {
+		const nextVal = Math.floor(p.progress * 100)
+		if (nextVal && nextVal > prev) {
+			bar.tick()
+			prev++
+		}
 	})
 	.catch(err => {
 		console.log(err)
