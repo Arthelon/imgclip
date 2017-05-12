fb944e4e7657a5f4a2ffaed3ead32ad0640269cb
Daniel Hsing <hsing.daniel@gmail.com>
2017-05-12 18:27:57 +0800

    more functional code, remove print global
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 #! /usr/bin/env node
-const args = require("commander")
+const program = require("commander")
 const copyPaste = require("copy-paste")
 const Tesseract = require("tesseract.js")
 const Progress = require("progress")
 const PkgJson = require("./package.json")
+const fs = require("fs");
 const langs = ["afr", "ara", "aze", "bel", "ben", "ben", "bul", "cat", "ces", "chi_sim", "chi_tra", "chr", "dan", "deu", "ell", "eng", "enm", "epo", "epo_alt", "equ", "est", "eus", "fin", "fra", "frk", "frm", "glg", "grc", "heb", "hin", "hrv", "hun", "ind", "isl", "ita", "ita_old", "jpn", "kan", "kor", "lav", "lit", "mal", "mkd", "mlt", "msa", "nld", "nor", "pol", "por", "ron", "rus", "slk", "slv", "spa", "spa_old", "sqi", "srp", "swa", "swe", "tam", "tel", "tgl", "tha", "tur", "ukr", "vie"]
-let print = false
 
-//Used in help
-args.usage("PATH [options]")
-args.description(PkgJson.description)
-args.version(PkgJson.version)
+program
+	.usage("PATH [options]")
+	.description(PkgJson.description)
+	.version(PkgJson.version)
+	.option("-l, --lang [language]", "language of the text in the image.")
+	.option("-p, --print", "prints out the text in the image.\n\nFull language list can be found here: \nhttps://github.com/naptha/tesseract.js/blob/master/docs/tesseract_lang_list.md")
+	.parse(process.argv)
 
-//Arguments (Order determines order shown in help)
-args.option("-l, --lang [language]", "language of the text in the image.")
-    .option("-p, --print", "prints out the text in the image."+
-      "\n\nFull language list can be found here: \nhttps://github.com/naptha/tesseract.js/blob/master/docs/tesseract_lang_list.md")
-args.parse(process.argv)
-
-if(!args.args.length || args.args[0] === undefined){
-	console.error("  No Path Specified")
-	args.help()
+const errorMessage = validateArgs(program)
+if (errorMessage) {
+	console.error(`\n${errorMessage}`)
+	program.help()
 	return
 }
+recognize(program.args[0], program.lang, program.print)
 
-if(!args.lang) {
-  args.lang = "eng"
+function validateArgs(args) {
+	if (!args.args.length || !args.args[0]){
+		return "No Path Specified"
+	}
+
+	if (args.lang && langs.indexOf(args.lang) === -1) {
+		return "Invalid Language!"
+	}
+
+	if (!fs.existsSync(args.args[0])) {
+		return `File path not found: ${args.args[0]}`;
+	}
+	return null;
 }
 
-if(langs.indexOf(args.lang) > -1) {
-  recognize(args.args[0], args.lang)
-} else {
-  console.log("  Invalid Language!")
-  args.help()
-  return
-}
-
-if(args.print)
-  print = true
-
-function recognize(imagePath, lang) {
+function recognize(imagePath, lang = 'eng', printResult = false) {
 	const bar = new Progress("recognizing [:bar] :percent :elapseds", {total: 100})
 	let prev = 0
 	Tesseract.recognize(imagePath, {
-		lang
+		lang,
+		tessedit_create_txt: false,		
 	})
 	.progress(p => {
 		const nextVal = Math.floor(p.progress * 100)
@@ -60,8 +60,9 @@ function recognize(imagePath, lang) {
 			bar.tick(100 - prev)
 		}
 		copyPaste.copy(result.text, () => {
-      		if(print)
-				console.log("\nResult:\n" + result.text.slice(0, result.text.length - 1))
+			if(printResult) {
+				console.log("\nResult:\n" + result.text.slice(0, result.text.length - 1))				
+			}
 			console.log("Finished copying to clipboard!")
 			process.exit()
 		})
