4d980937afd83207d6a40ac319ba0c3fe173a493
Daniel Hintz <danielchintz@gmail.com>
2019-04-21 02:35:42 -0400

    added multi file support, probably going to add support to send the text to respective text files for each image

--- index.js
+++ index.js
@@ -8,72 +8,85 @@ const fs = require("fs");
 const langs = ["afr", "ara", "aze", "bel", "ben", "ben", "bul", "cat", "ces", "chi_sim", "chi_tra", "chr", "dan", "deu", "ell", "eng", "enm", "epo", "epo_alt", "equ", "est", "eus", "fin", "fra", "frk", "frm", "glg", "grc", "heb", "hin", "hrv", "hun", "ind", "isl", "ita", "ita_old", "jpn", "kan", "kor", "lav", "lit", "mal", "mkd", "mlt", "msa", "nld", "nor", "pol", "por", "ron", "rus", "slk", "slv", "spa", "spa_old", "sqi", "srp", "swa", "swe", "tam", "tel", "tgl", "tha", "tur", "ukr", "vie"]
 
 program
-	.usage("PATH [options]")
-	.description(PkgJson.description)
-	.version(PkgJson.version)
-	.option("-l, --lang [language]", "language of the text in the image.")
-	.option("-c, --clean-up", "removes the generated language data file (.traineddata) after the image recognition job has finished")	
-	.option("-p, --print", "prints out the text in the image.\n\nFull language list can be found here: \nhttps://github.com/naptha/tesseract.js/blob/master/docs/tesseract_lang_list.md")
-	.parse(process.argv)
+    .usage("PATH [options]")
+    .description(PkgJson.description)
+    .version(PkgJson.version)
+    .option("-l, --lang [language]", "language of the text in the image.")
+    .option("-c, --clean-up", "removes the generated language data file (.traineddata) after the image recognition job has finished")
+    .option("-p, --print", "prints out the text in the image.\n\nFull language list can be found here: \nhttps://github.com/naptha/tesseract.js/blob/master/docs/tesseract_lang_list.md")
+    .parse(process.argv)
 
 const errorMessage = validateArgs(program)
 if (errorMessage) {
-	console.error(`\n${errorMessage}`)
-	program.help()
-	return
+    console.error(`\n${errorMessage}`)
+    program.help()
+    return
 }
-recognize({
-	imagePath: program.args[0], // file path
-	lang: program.lang,
-	printResult: program.print,
-	cleanup: program.cleanUp,
-})
+
+start()
+
+async function start() {
+    for (var i = 0; i < program.args.length;i++) {
+        await recognize({
+            imagePath: program.args[i], // file path
+            lang: program.lang,
+            printResult: program.print,
+            cleanup: program.cleanUp,
+        })
+    }
+    //sleep so the process has time to print everything out
+    await sleep(1000)
+    process.exit()
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function validateArgs(args) {
-	if (!args.args.length || !args.args[0]){
-		return "No Path Specified"
-	}
+    if (!args.args.length || !args.args[0]){
+        return "No Path Specified"
+    }
 
-	if (!fs.existsSync(args.args[0])) {
-		return `File path not found: ${args.args[0]}`;
-	}
+    if (!fs.existsSync(args.args[0])) {
+        return `File path not found: ${args.args[0]}`;
+    }
 
-	if (args.lang && langs.indexOf(args.lang) === -1) {
-		return "Invalid Language!"
-	}
-	return null;
+    if (args.lang && langs.indexOf(args.lang) === -1) {
+        return "Invalid Language!"
+    }
+    return null;
 }
 
-function recognize({ imagePath, lang = 'eng', printResult = false, cleanup = false }) {
-	const bar = new Progress("recognizing [:bar] :percent :elapseds", {total: 100})
-	let prev = 0
-	Tesseract.recognize(imagePath, {
-		lang,
-		tessedit_create_txt: false,		
-	})
-	.progress(p => {
-		const nextVal = Math.floor(p.progress * 100)
-		if (nextVal && nextVal > prev) {
-			bar.tick()
-			prev++
-		}
-	})
-	.catch(err => {
-		console.log(err)
-	})
-	.then(result => {
-		if (prev < 100) {
-			bar.tick(100 - prev)
-		}
-		if (cleanup) {
-			fs.unlinkSync(`${lang}.traineddata`)
-		}
-		copyPaste.copy(result.text, () => {
-			if(printResult) {
-				console.log("\nResult:\n" + result.text.slice(0, result.text.length - 1))				
-			}
-			console.log("Finished copying to clipboard!")
-			process.exit()
-		})
-	})
+async function recognize({ imagePath, lang = 'eng', printResult = false, cleanup = false }) {
+    const bar = new Progress("recognizing [:bar] :percent :elapseds", {total: 100})
+    let prev = 0
+    await Tesseract.recognize(imagePath, {
+        lang,
+        tessedit_create_txt: false,
+    })
+        .progress(p => {
+            const nextVal = Math.floor(p.progress * 100)
+            if (nextVal && nextVal > prev) {
+                bar.tick()
+                prev++
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        .then(result => {
+            if (prev < 100) {
+                bar.tick(100 - prev)
+            }
+            if (cleanup) {
+                fs.unlinkSync(`${lang}.traineddata`)
+            }
+            copyPaste.copy(result.text, () => {
+                if(printResult) {
+                    console.log("\nResult:\n" + result.text.slice(0, result.text.length - 1))
+                }
+                console.log("Finished copying to clipboard!")
+            })
+        })
 }
